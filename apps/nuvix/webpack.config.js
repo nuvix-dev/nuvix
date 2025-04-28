591813aedfc843ca9cb82d4da7253642dec6cb44
Ravikant Saini <ravikantsaini047@gmail.com>
2025-04-28 12:13:39 +0530

    SOME CHNAGES THAT DOSEN"T MATTER

---
+++ apps/nuvix/webpack.config.js
@@ -0,0 +1,29 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
+
+module.exports = (env) => {
+    const mode = env.NODE_ENV || 'production';
+    console.log(`Building in ${mode} mode, with source maps enabled`);
+
+    return {
+        mode,
+        output: {
+            path: join(__dirname, '../../dist/apps/nuvix'),
+        },
+        plugins: [
+            new NxAppWebpackPlugin({
+                target: 'node',
+                compiler: 'swc',
+                main: './src/main.ts',
+                tsConfig: './tsconfig.app.json',
+                assets: ['./src/assets'],
+                optimization: false,
+                outputHashing: 'none',
+                additionalOptions: {
+                    sourceMap: true,
+                },
+                externalDependencies: "all",
+            })
+        ],
+    };
+};
